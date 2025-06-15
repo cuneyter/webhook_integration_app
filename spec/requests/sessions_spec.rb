bdc8c582adaf7781b89d8c6e390e722171a24fe6
Cuneyt Ergun <cuneytergun@gmail.com>
2025-06-15 09:30:53 +0100

    Update sessions_spec.rb

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,8 +3,9 @@ require 'rails_helper'
 RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
     it "returns http success" do
-      get "/session/new"
+      get new_session_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include("Sign in")
     end
   end
 end
