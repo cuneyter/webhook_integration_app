9648b4312f13bbfa2002fe758861583cacd6c430
Cuneyt Ergun <cuneytergun@gmail.com>
2025-06-22 12:06:22 +0100

    refactor: Streamline Session model by removing unnecessary validations and methods

--- app/models/session.rb
+++ app/models/session.rb
@@ -16,18 +16,4 @@
 
 class Session < ApplicationRecord
   belongs_to :user
-
-  validates :user, presence: true
-  validates :ip_address, presence: true, if: :ip_address_required?
-  validates :user_agent, presence: true, if: :user_agent_required?
-
-  private
-
-  def ip_address_required?
-    false
-  end
-
-  def user_agent_required?
-    false
-  end
 end
